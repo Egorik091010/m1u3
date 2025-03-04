200df37bddf0d71f4e525b9b0bd5ed0cdd9e2814
egor <moiseenkovegorik09@gmail.com>
2025-03-04 11:21:52 +0100

    init project

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot # библиотека telebot
+import re
 from config import token # импорт токена
 
 bot = telebot.TeleBot(token) 
@@ -23,4 +24,28 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    # Проверка на наличие URL в сообщении
+    if re.match(r'https?://', message.text):  # Проверка на ссылку (http:// или https://)
+        if message.reply_to_message:
+            chat_id = message.chat.id
+            user_id = message.reply_to_message.from_user.id
+            user_status = bot.get_chat_member(chat_id, user_id).status
+            
+            if user_status == 'administrator' or user_status == 'creator':
+                bot.reply_to(message, "Невозможно забанить администратора.")
+            else:
+                bot.ban_chat_member(chat_id, user_id)  # Баним пользователя
+                bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+        else:
+            bot.reply_to(message, "Сообщение должно быть ответом на сообщение пользователя, которого нужно забанить.")
+    else:
+        bot.reply_to(message, message.text)
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+    
 bot.infinity_polling(none_stop=True)
